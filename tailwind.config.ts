5206c7d0d526ae2f59217f99aef0049fe220e960
KalenEdvard <155569725+KalenEdvard@users.noreply.github.com>
2026-04-02 20:41:19 +0600

    feat: tailwind with oswald font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: ['var(--font-oswald)', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
